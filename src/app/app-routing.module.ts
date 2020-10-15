91ade91deb39da0448c14addf14c46c2c6d2c3f8
leobaz <leutrim.kosumi@webtiser.ch>
2020-10-15 17:51:56 +0200

    create components, add navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateComponent } from './location/create/create.component';
+import { DetailsComponent } from './location/details/details.component';
+import { LocationComponent } from './location/location.component';
+import { UpdateComponent } from './location/update/update.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LocationComponent },
+  { path: ':id', component: DetailsComponent },
+  { path: 'create', component: CreateComponent},
+  { path: 'update/:id', component: UpdateComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LocationComponent } from './location/location.component';
+import { ListComponent } from './location/list/list.component';
+import { DetailsComponent } from './location/details/details.component';
+import { CreateComponent } from './location/create/create.component';
+import { UpdateComponent } from './location/update/update.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocationComponent,
+    ListComponent,
+    DetailsComponent,
+    CreateComponent,
+    UpdateComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
